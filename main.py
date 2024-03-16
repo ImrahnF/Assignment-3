3f7ce6fa3220c004fcb88fa03c20f00ddd5459cc
Potato <imrahnfaqiri21@gmail.com>
2024-03-16 18:10:12 -0400

    Milestone 1
    Created project and created a basic outline on what classes will be used

---
+++ main.py
@@ -0,0 +1,31 @@
+'''
+main.py. This is where all interactions and functions occur.
+
+--// Class functions //--
+
+--// Other functions //--
+
+start():
+- start the program
+'''
+
+class Account:
+    pass  # Implement methods as per the requirements
+
+class SavingsAccount(Account):
+    pass
+
+class ChequingAccount(Account):
+    pass
+
+class Bank:
+    pass
+
+class Program:
+    pass
+
+# Main function to run the program
+def start():
+    pass
+
+start()
